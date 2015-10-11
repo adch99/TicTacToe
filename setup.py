df48b4ce6fee29df9b2a4162cd4d40e102cfe9c9
adch99 <adityachincholi@gmail.com>
2015-10-11 22:10:31 +0530

    Updated setup

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ except ImportError:
 	from distutils.core import setup
 config = {
 	'description': 'A game of Tic-Tac-Toe',
-	'author': 'Aditya',
-	'url': 'url to get it at',
+	'author': 'Aditya Chincholi',
+	'url': 'https://github.com/adch99/TicTacToe.git',
 	'download_url': 'Where to download it',
 	'author_email': 'adityachincholi@gmail.com',
 	'version': '0.1',
 	'install_requires': ['nose'],
 	'packages': ['TicTacToe'],
-	'scripts': ['game.py','player.py',"main.py"],
+	'scripts': ['engine.py', 'player.py', "Comp.py", "main.py", "Compmain.py"],
 	'name': 'TicTacToe'
 }
 
